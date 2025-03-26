6ecab72c7e8894dc1506f9f0006de0fc4f9a57b5
Carl0s26 <Carloskepp26@gmail.com>
2025-03-26 13:28:40 -0400

    Added Slow print
    Now the Ai (I don't remember the name) gives its outputs slowly.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import flet as ft 
 from openai import OpenAI
-client = OpenAI(api_key="THE API KEY")
+import time as time
+client = OpenAI(api_key="")
 #! REMEMBER TO DELETE API KEY BEFORE PUSHING TO GITHUB!!!!!!!!!
+
 
 
 def main(page: ft.Page):
     page.title = "Chat with JASC"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    def slowPrint(textField, text):
+        for i in range(len(text)):
+            textField.value = f"{textField.value}{text[i]}"
+            time.sleep(0.005)
+            page.update()
 
     #!Area con el chat
     chat_area = ft.Column(
@@ -14,11 +22,17 @@ def main(page: ft.Page):
         expand=True,
         spacing=10,
     )
+    
+    #!entrada
+    input_field = ft.TextField(
+        hint_text="Write your message",
+        expand=True,
+    )
 
     #! Botón de envío
     send_button = ft.IconButton("Send")
 
-    #! Función para el envío de mensajes
+    #! Función para el envío y respuesta de mensajes
     def send_message(e):
         if input_field.value:
             chat_area.controls.append(ft.Text(f"You: {input_field.value}"))
@@ -30,19 +44,18 @@ def main(page: ft.Page):
                 {"role": "user", "content": f"{input_field.value}"}
             ]
             )
-            chat_area.controls.append(ft.Text(f"JASC: {completion.choices[0].message.content}")) # ai's response
-            #! Aquí iría la lógica para obtener la respuesta de la IA
+            chat_area.controls.append(ft.Text(f"JASC: ")) # ai's response
+            slowPrint(chat_area.controls[-1],completion.choices[0].message.content)
+
             # chat_area.controls.append(ft.Text(f"AI: Simulated response"))
             input_field.value = ""
             page.update()
-    
-    #!entrada
+
     input_field = ft.TextField(
         hint_text="Write your message",
         expand=True,
         on_submit=send_message
     )
-
     send_button.on_click = send_message
     
     page.add(
@@ -50,7 +63,7 @@ def main(page: ft.Page):
             controls=[
                 ft.Container(
                     content=chat_area,
-                    border=ft.border.all(1, ft.colors.OUTLINE),
+                    border=ft.border.all(1, ft.Colors.OUTLINE),
                     border_radius=5,
                     padding=10,
                     expand=True,
